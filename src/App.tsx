d66ecf93108a7c10501c925062c69dea34149ea6
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-11 10:24:25 +0200

    building components: passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import ListGroup from "./components/ListGrup";
+import Alert from "./components/Alert";
 
 function App() {
-  const items = [
-    "New York",
-    "San Francisco",
-    "London",
-    "Beijing",
-    "Shenzhen",
-    "Zurich",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
-
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      <Alert>
+        Hello <span>Mars</span>
+      </Alert>
     </div>
   );
 }
